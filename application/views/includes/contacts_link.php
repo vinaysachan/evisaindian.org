3ba54aadd32c4bd54f0ea3bb7b3e2fafe13f156f
Vinay Sachan <vinay@onlinephpstudy.com>
2017-11-18 22:50:40 +0530

    logo add

--- application/views/includes/contacts_link.php
+++ application/views/includes/contacts_link.php
@@ -1,5 +1,6 @@
 <div class="right-box"> 
-    <h3><i class="fa fa-phone fs20 mr10"></i> 24/7 Customer Care</h3>
+    <span id="siteseal"><script async type="text/javascript" src="https://seal.godaddy.com/getSeal?sealID=q7JusASCPbMlx4UAp3OkMoYCA35ueeUtNYUTdOQviKH4UActz065EsHc6GL0"></script></span>
+    <h3 class="mt15"><i class="fa fa-phone fs20 mr10"></i> 24/7 Customer Care</h3>
     <div class="right-box-content"> 
         <ul class="blog-popular-post">
             <li class=""><img src="<?=base_url('public/img/uk.png')?>" class="mr20" >UK <?= CONTACT_NO_UK ?></li>
