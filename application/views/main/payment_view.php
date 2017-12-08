47b8074b1c3b370bb0b609e9c89a95dc2ee3c119
Vinay Sachan <vinay@onlinephpstudy.com>
2017-12-08 23:14:55 +0530

    css chnage

--- application/views/main/payment_view.php
+++ application/views/main/payment_view.php
@@ -17,21 +17,25 @@
                 <td>Amount (USD)</td>
                 <td>$<?= apptype_amount($apply_details[0]->app_type); ?></td>
             </tr>
+            <tr>
+                <td><input type="image" name="submit" border="0" src="<?= base_url('public/img/paypal.png') ?>" alt="PayPal - The safer, easier way to pay online" height="30"/></td>
+                <td>
+                    <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+                        <!-- <input type="hidden" name="cmd" value="_cart"> -->
+                        <input type="hidden" name="cmd" value="_xclick">
+                        <input type="hidden" name="business" value="touristvisa701@gmail.com">
+                        <input type="hidden" name="item_name" value="<?= apptype($apply_details[0]->app_type); ?>">
+                        <input type="hidden" name="item_number" value="<?= $this->session->userdata('application_id'); ?>">
+                        <input type="hidden" name="amount" value="<?= apptype_amount($apply_details[0]->app_type); ?>">
+                        <input type="hidden" name="currency_code" value="USD">
+                        <input type='hidden' name='cancel_return' value='<?= base_url('main/payment_faild') ?>'>
+                        <input type='hidden' name='return' value='<?= base_url('main/payment_success') ?>'>
+                        <!--<input type="image" name="submit" border="0" src="<?= base_url('public/img/paypal.png') ?>" alt="PayPal - The safer, easier way to pay online" width="24%" height="66"/>-->
+                        <img alt="" border="0" width="1" height="1" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" >
+                        <button type="submit" alt="PayPal - The safer, easier way to pay online" href="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" class="btn btn-warning pl30 pr30">Pay With Paypal</button>
+                    </form>
+                </td>
+            </tr>
         </table>
-        <form class="text-center" action="https://www.paypal.com/cgi-bin/webscr" method="post">
-            <!-- <input type="hidden" name="cmd" value="_cart"> -->
-            <input type="hidden" name="cmd" value="_xclick">
-            <input type="hidden" name="business" value="touristvisa701@gmail.com">
-            <input type="hidden" name="item_name" value="<?= apptype($apply_details[0]->app_type); ?>">
-            <input type="hidden" name="item_number" value="<?= $this->session->userdata('application_id'); ?>">
-            <input type="hidden" name="amount" value="<?= apptype_amount($apply_details[0]->app_type); ?>">
-            <input type="hidden" name="currency_code" value="USD">
-            <input type='hidden' name='cancel_return' value='<?= base_url('main/payment_faild') ?>'>
-            <input type='hidden' name='return' value='<?= base_url('main/payment_success') ?>'>
-            <input type="image" name="submit" border="0" src="<?= base_url('public/img/paypal.png') ?>" alt="PayPal - The safer, easier way to pay online" width="24%" height="66"/>
-            <img alt="" border="0" width="1" height="1" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" >
-        </form>
-
-
     </div>
 </div>  
